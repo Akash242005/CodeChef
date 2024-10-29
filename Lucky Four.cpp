ddd0b5383ecdba8648e7de6862eec95362ef98ea
AKASH B <147173640+Akash242005@users.noreply.github.com>
2024-10-29 19:19:54 +0530

    Create Lucky Four.cpp

---
+++ Lucky Four.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	int count =0;
+	for(int i=0;i<t;i++)
+	{
+	    string a;
+	    cin >> a;
+	    int b = a.length();
+	    for(int j=0;j< b;j++)
+	    {
+	        if(a[j] == '4')
+	        {
+	            count++;
+	        }
+	    }
+	    cout << count <<endl;
+	    count =0;
+	}
+	return 0;
+}
